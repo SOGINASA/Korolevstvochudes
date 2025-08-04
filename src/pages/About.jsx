edc143cb86ea94aa7945e88bbb7960c3efd89621
SOGINASA <artempinigin2008@gmail.com>
2025-08-04 15:28:57 +0500

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+// pages/About.jsx
+const About = () => (
+    <div>
+      <h1>О нас</h1>
+      <p>Это страница "О нас"</p>
+    </div>
+  )
+  
+  export default About
+  
---
+++ src/routes/index.jsx
@@ -0,0 +1,20 @@
+// routes/index.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import NotFound from '../pages/NotFound'
+import MainLayout from '../layouts/MainLayout'
+
+const AppRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+)
+
+export default AppRoutes
